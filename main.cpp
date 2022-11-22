364c2bc8e13381ddd5f89b1d26e54f7a0241e1d0
AndreaGenovese03 <118006599+AndreaGenovese03@users.noreply.github.com>
2022-11-22 16:35:10 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,21 @@
 using namespace std;
 
 int main() {
+   int n;
+    int a=0, b=1, c;
+
+    cout << "Quanti numeri della serie vuoi stampare? : ";
+    cin >> n;
+
+    if (n>=2){
+        for (int i=0; i<n; i++){
+            cout << a << endl;
+            c=a;
+            a=b;
+            b=a+c;
+        }
+    }else{
+        cout << "errore";
+    }
    return 0;
 }
